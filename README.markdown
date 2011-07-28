8f8b48013ae97f1d754dbdd7292843d657a3201f
Andrew Moist <andyjm@gmail.com>
2011-07-28 17:21:34 +0100

    removed swf_object dependency. using new iframe api

--- README.markdown
+++ README.markdown
@@ -1,14 +1,18 @@
-img2youtube
------------------------------------
+# img2youtube
 
 Get full control over the first image on the video.
 
-This jQuery plugin turns an image into a YouTube video when you click on it.
+This jQuery plugin turns an image into a YouTube video when you click on it. It uses the YouTube <iframe> Player API.
 
 Features:
-- Progressive enhancement. If JavaScript isn't enabled the image will link to the YouTube page. If flash isn't available, HTML5 <video> is used.
-- Auto-play video on click (only works if flash is installed)
+- Progressive enhancement. If JavaScript isn't enabled the image will link to the YouTube page.
+- YouTube iFrame works with HTML5 and flash.
+- Auto-play video on click
 
 Known issues:
-- Auto-play doesn't work if flash isn't enabled
- 
+- Google API is currently an experimental feature, which means that it might change unexpectedly.
+- A JavaScript warning error is logged to the console. This does not affect any functionality.
+The error is: "Unsafe JavaScript attempt to access frame with URL [url] from frame with URL [url]. Domains, protocols and ports must match."
+
+You can read the YouTube <iframe> Player API reference here:
+http://code.google.com/apis/youtube/iframe_api_reference.html
--- js/jquery.img2youtube.js
+++ js/jquery.img2youtube.js
@@ -1,78 +1,66 @@
+//Load player api asynchronously.
+var tag = document.createElement('script');
+tag.src = "http://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// Please note: You must run this on a web server to work. Running it from the local file system will not work.
-var onYouTubePlayerReady = function (playerId) {
+// jQuery img2youtube plugin.
+(function($){
+
+  $.fn.img2youtube = function(options){
   
-  var ytplayer = document.getElementById(playerId);
-  ytplayer.playVideo();
-};
-
-(function( $ ){
-
-  $.fn.img2youtube = function(options) {  
-
     var settings = {
-      'method' : 'iframe',
-      'autoplay' : true
+      'autoplay': true
     };
-
-    $.extend( settings, options );
-    if (! settings.autoplay) {
-      onYouTubePlayerReady = null;
-    }
+    $.extend(settings, options);
     
-    var replaceWithIframe = function(element, width, height, youtube_id) {
+    var replaceWithVideo = function (event, element_id) {
       
-      var html = $( '<iframe width="'+ width +'" height="'+ height +'" src="http://www.youtube.com/embed/'+ youtube_id +'" frameborder="0" allowfullscreen></iframe>');
+      event.preventDefault();
       
-      $(element)
+      var self = event.currentTarget;
+      
+      var youtube_url = $(self).attr('href');
+      if (youtube_url === undefined) {
+        console.log('Error: Invalid YouTube video link. Link should be in the format: http://youtu.be/CODE-HERE');
+        return;
+      }
+      
+      var videoId = youtube_url.substr(youtube_url.lastIndexOf('/') + 1, youtube_url.length);
+      var img = $(self).find('img:first');
+      var width = $(img).attr('width');
+      var height = $(img).attr('height');
+  
+      var html = '<div id="' + element_id + '"></div>';
+      
+      $(self)
         .after(html)
         .remove();
-    } 
 
-    var replaceWithFlash = function(element, width, height, youtube_id) {
-      
-        var id = "video_" + youtube_id;
-        var html = $( '<div id="'+ id +'" />');
-      
-        $(element)
-          .after(html)
-          .remove();
-      
-        var params = { allowScriptAccess: "always", allowFullScreen: "true" };
-        var atts = { id: id };
-        swfobject.embedSWF('http://www.youtube.com/v/' + youtube_id +'?version=3&enablejsapi=1&feature=player_embedded&playerapiid=' + id,
-                           id, width, height, "8", null, null, params, atts);
-    }
-
-    return this.each(function() {        
-
-      $(this).click(function(event) {
-        
-        event.preventDefault();
-  
-        var youtube_url = $(this).attr('href');
-        if (youtube_url === undefined) {
-          console.log('Error: Invalid YouTube video link. Link should be in the format: http://youtu.be/vdQj2ohqCBk');
-          return;
-        }
-        
-        var youtube_id = youtube_url.substr(youtube_url.lastIndexOf('/')+1, youtube_url.length);
-        
-        var img = $(this).find('img:first');
-        var width = $(img).attr('width');
-        var height = $(img).attr('height');
-        
-        if (typeof(swfobject) !== "undefined" && swfobject.hasFlashPlayerVersion("8") && settings.method == 'swfobject') {
-
-          replaceWithFlash(this, width, height, youtube_id);
-        
-        } else {
-          
-          replaceWithIframe(this, width, height, youtube_id);
+      new YT.Player(element_id, {
+        height: height,
+        width: width,
+        videoId: videoId,
+        events: {
+          'onReady': function(evt){
+            if (settings.autoplay) {
+              evt.target.playVideo();  
+            }
+          }
         }
       });
-      
+    }
+    
+    var i = 0;
+    return this.each(function(){
+    
+      $(this).click(function(event) {
+        
+      	replaceWithVideo(event, 'video_' + i);
+        i++;
+      });
     });
-
+    
   };
-})( jQuery );
+})(jQuery);
+
